82c6933fef37d8bf9f5ae6026f1416313dde4746
2029698 <2029698@TE-GDA-D-D21E24>
2024-09-16 12:25:02 +0200

    Dodaj pliki projektów.

---
+++ DiceRollkc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DiceRollkc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DiceRollkc/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace DiceRollkc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRollDiceClicked(object sender, EventArgs e)
+        {
+            var random = new Random();
+            int result = random.Next(1, 7); // K6
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
+
